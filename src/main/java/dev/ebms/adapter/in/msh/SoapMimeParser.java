336c45d86c3a1449fdfdc40d350982790afc2243
rooimoer47 <phggrabe@gmail.com>
2026-05-23 08:28:20 +0200

    Fix XXE vulnerability in SoapMimeParser

--- src/main/java/dev/ebms/adapter/in/msh/SoapMimeParser.java
+++ src/main/java/dev/ebms/adapter/in/msh/SoapMimeParser.java
@@ -118,6 +118,11 @@ public class SoapMimeParser {
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setNamespaceAware(true);
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            factory.setXIncludeAware(false);
+            factory.setExpandEntityReferences(false);
             DocumentBuilder builder = factory.newDocumentBuilder();
             return builder.parse(new InputSource(is));
         } catch (Exception e) {
